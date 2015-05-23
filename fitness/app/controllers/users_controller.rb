17c2d4b22b9c4c19f77f52fb8e0136b4fa92e9b3
yyq410 <1010676806@qq.com>
2015-05-23 10:39:17 +0800

    game over~

--- fitness/app/controllers/users_controller.rb
+++ fitness/app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
     def create
 	   @user = User.new(user_params)
+     @user[:is_administrator] = "ordinary"
 
 	   if @user.save		
            redirect_to :controller => 'homepage', :action => :index
